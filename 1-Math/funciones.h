9bd87f47b0db04ccb39f45bc1420917826b0e10e
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2022-08-02 12:30:47 -0300

    Create funciones.h

---
+++ 1-Math/funciones.h
@@ -0,0 +1,23 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+double pow_MIO(double base, double exponente);
+long long int factorial(unsigned num);
+unsigned combinatoriaMsobreN(unsigned m, unsigned n);
+double exponencialE(int num, float tol);
+int absoluto(int num);
+int perteneceAFibonacci(unsigned num);
+double senX(int num, float tol);
+void mostrarClasificacion(int num);
+int calcularClasificacion(int num);
+int productosPorSuma(int multiplicando, int multiplicador);
+
+int cocienteEntero(int dividendo, int divisor);
+int sumaNumerosNaturales(int num);
+int sumaNumerosNaturalesPares(int num);
+int esNumeroPrimo(int num);
+int multiplicacionRusa(int num1, int num2);
+int parteEntera(double num);
+
+#endif // FUNCIONES_H_INCLUDED
