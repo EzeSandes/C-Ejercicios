3eaf1359a30ef74155a4de6b1a2220996b361ad6
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2022-10-21 14:31:13 -0300

    Update 5-strncmp.c

--- 4-Cadenas/Librerias-StringH-CtypeH/5-strncmp.c
+++ 4-Cadenas/Librerias-StringH-CtypeH/5-strncmp.c
@@ -1,9 +1,10 @@
-int str_cmp(const char *cad1, const char *cad2)
+int str_ncmp(const char *cad1, const char *cad2, size_t n)
 {
-    while(*cad1 && *cad1 == *cad2)
+    while(*cad1 == *cad2 && *cad1 && n)
     {
         cad1++;
         cad2++;
+        n--;
     }
 
     return *(unsigned char*)cad1 - *(unsigned char*)cad2;
