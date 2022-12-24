1a010796d485bc5ce7ede771e65e55bb95c0402c
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2022-12-24 10:40:00 -0300

    Update 4-Atoi.c

--- 4-Cadenas/Cadenas-Ejercicios/4-Atoi.c
+++ 4-Cadenas/Cadenas-Ejercicios/4-Atoi.c
@@ -35,3 +35,63 @@ int atoi_mio(const char *numPtr)
 
     return numero * signo;
 }
+
+
+/////////////////////////// PROBADOR ATOI
+
+#define COL_CHARS 50
+
+   char pruebasAtoi[][50] = {
+                           "    123  565 l",
+                           "    -123L",
+                           "    LABV123",
+                           "    LABV-123",
+                           "    LABV+123",
+                           "    123ABC",
+                           "    123 ABC",
+                           "    ABC",
+                           "    ",
+                           "    ?//123",
+                           "    ?//+123",
+                           "    ?//-123",
+                           "-1234",
+                           "+1234",
+                           "",
+                           "    +12345",
+                           "    -12345",
+                           "    12345",
+                           "    12345   ABC",
+                           "    -12345   ABC",
+                           "    +12345   ABC",
+                           "    +1234567890",
+                           "    -1234567890",
+                           "    +12345678900", // Se pasan AMBOS del rango permitido
+                           "    -12345678900", // Ya hay error en ambas funciones.
+                           // Soportan numeros de hasta 10 Digitos.
+                           "abc",
+                           "a1b2c3",
+                           "  000123",
+                           "  -000123",
+                           "  +000123",
+                           "000123",
+                           "-000123",
+                           "+000123",
+                       };
+
+probadorAtoi(pruebasAtoi, sizeof(pruebasAtoi) / 50);
+
+///////////////
+
+void probadorAtoi(char pruebas[][COL_CHARS], int filas)
+{
+    int i;
+
+    for(i = 0; i < filas; i++)
+    {
+        printf("Atoi <stdlib.h> ( %s ): %d", pruebas[i], atoi(pruebas[i]));
+        printf("\nAtoi <MIO> ( %s ): %d", pruebas[i], atoi_mio(pruebas[i]));
+
+        printf("\n\n");
+    }
+}
+
