ba7fb9960c38286cb61c3389ebe19d4fe59f250d
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2023-05-03 10:21:16 -0300

    Create funciones.h

---
+++ 7-MemoriaDinamica/1-Ejercicio-MemDin/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void mi_memcpy(void *destino, const void *origen, size_t cant);
+void* mi_memmove(void *destino, const void *origen, size_t cant);
+
+#endif // FUNCIONES_H_INCLUDED
