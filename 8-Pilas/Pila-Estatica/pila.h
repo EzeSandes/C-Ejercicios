87a6b24986ec8dbd25bdbdb1071e09da1862a4d4
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2023-06-13 19:50:47 -0300

    Create pila.h

---
+++ 8-Pilas/Pila-Estatica/pila.h
@@ -0,0 +1,31 @@
+#ifndef PILA_H_INCLUDED
+#define PILA_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM_PILA 200
+
+/////////// PILA ESTATICA
+
+typedef struct
+{
+    char        pila[TAM_PILA];
+    unsigned    tope;
+}tPila;
+
+///////////
+
+void crearPila(tPila *p);
+int apilar(tPila *p, const void *d, unsigned cantBytes);
+int desapilar(tPila *p, void *d, unsigned cantBytes);
+int pilaVacia(const tPila *p);
+int pilaLlena(const tPila *p, unsigned cantBytes); // No tiene sentido con pila de memoria dinamica ya que
+// en un microSeg otro proceso me toma la mem q tome anteriormente. Se debe quedar
+// para que sea compatible con la implementacion Estatica de Pila.
+// Sirve para mantener la compatibilidad con pila estatica.
+void vaciarPila(tPila *p);
+int verTope(const tPila *p, void *d, unsigned cantBytes);
+
+#endif // PILA_H_INCLUDED
