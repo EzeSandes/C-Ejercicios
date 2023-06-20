14e5d6b5b794a8f7fd324ee837f4370f440f838f
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2023-06-20 12:23:15 -0300

    Create nodo.h

---
+++ 9-Cola/Cola-Dinamica/nodo.h
@@ -0,0 +1,11 @@
+#ifndef NODO_H_INCLUDED
+#define NODO_H_INCLUDED
+
+typedef struct sNodo
+{
+    void            *info;
+    unsigned        tamInfo;
+    struct sNodo    *sig;
+}tNodo;
+
+#endif // NODO_H_INCLUDED
